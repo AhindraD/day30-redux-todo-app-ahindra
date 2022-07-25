433dcb12cbc4a436cd0d5be3aebcf8725ff0579c
Ahindra D <official.ahindra@gmail.com>
2022-07-25 14:51:50 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from "react";
+import TodoList from "./components/TodoList";
+
+//'/images/icon-sun.svg'
+//{state.theme === 'light' ? '/images/icon-sun.svg' : '/images/icon-moon.svg'}
+
+const ACTIONS = {
+  THEME: 'update-theme',
+  LIST: 'add-to-list',
+  DONE: 'mark-task-complete',
+  DEL: 'clear-task',
+  deleteCOMPLETED: 'delete-completed-tasks'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+
+    case ACTIONS.THEME:
+      if (state.theme === 'light') {
+        return { ...state, theme: 'dark' }
+      } else {
+        return { ...state, theme: 'light' }
+      }
+
+    case ACTIONS.LIST:
+      let task = action.payload;
+      let ID = new Date().getTime();
+      state.list.push({ ID: ID, task: task, isComplete: false });
+      state.pending.push({ ID: ID, task: task, isComplete: false });
+      return { ...state };
+
+    case ACTIONS.DONE:
+      for (let i = 0; i < state.list.length; i++) {
+        let currID = state.list[i].ID;
+        if (currID === action.payload) {
+          state.list[i].isComplete = true;
+          state.complete.push(state.list[i]);
+          break;
+        }
+      }
+      for (let i = 0; i < state.pending.length; i++) {
+        let currID = state.pending[i].ID;
+        if (currID === action.payload) {
+          state.pending.splice(i, 1);
+          break;
+        }
+      }
+      return { ...state };
+
+    case ACTIONS.DEL:
+      for (let i = 0; i < state.list.length; i++) {
+        let currID = state.list[i].ID;
+        if (currID === action.payload) {
+          state.list.splice(i, 1);
+          break;
+        }
+      }
+      for (let i = 0; i < state.pending.length; i++) {
+        let currID = state.pending[i].ID;
+        if (currID === action.payload) {
+          state.pending.splice(i, 1);
+          break;
+        }
+      }
+      for (let i = 0; i < state.complete.length; i++) {
+        let currID = state.complete[i].ID;
+        if (currID === action.payload) {
+          state.complete.splice(i, 1);
+          break;
+        }
+      }
+      return { ...state };
+
+    case ACTIONS.deleteCOMPLETED:
+      for (let i = 0; i < state.list.length; i++) {
+        if (state.list[i].isComplete) {
+          state.list.splice(i, 1);
+          i--;
+        }
+      }
+      state.complete = [];
+      return { ...state };
+
+    default:
+      return state;
+  }
+}
+
+
 
 function App() {
+
+  let [state, dispatch] = useReducer(reducer, { theme: 'dark', list: [], pending: [], complete: [] });
+  let [filter, setFilter] = useState('list');
+
+  function markDone(ID) {
+    console.log(ID);
+    dispatch({ type: ACTIONS.DONE, payload: ID })
+  }
+
+  function clearTask(ID) {
+    console.log(ID);
+    dispatch({ type: ACTIONS.DEL, payload: ID })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${state.theme}-cont`}>
+      <div className="cover-pic"></div>
+      <main className={`card ${state.theme}`}>
+
+        <div className={`header ${state.theme}`}>
+          <p className="title">TODO</p>
+          <button className="mode" onClick={() => {
+            dispatch({ type: ACTIONS.THEME });
+          }}>
+            <img className="icon" src={state.theme === 'light' ? '/images/icon-moon.svg' : '/images/icon-sun.svg'} alt="" />
+          </button>
+        </div>
+
+        <input type="text" placeholder="Create a new todo" className={`input-bar ${state.theme}`} onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            dispatch({ type: ACTIONS.LIST, payload: e.target.value });
+            e.target.value = '';
+          }
+        }}
+        />
+
+        <section className={`list ${state.theme}`}>
+          {state[filter].map((elem) => { return <TodoList key={elem.ID} ID={elem.ID} task={elem.task} isComplete={elem.isComplete} handleDone={markDone} handleDel={clearTask} /> })}
+        </section>
+
+        <footer className={`filters ${state.theme}-foot`}>
+          <p className="left">{`${state.pending.length} tasks left`}</p>
+
+          <button className="all" onClick={() => {
+            setFilter('list');
+          }}>All</button>
+
+          <button className="active" onClick={() => {
+            setFilter('pending');
+          }}>Active</button>
+
+          <button className="completed" onClick={() => {
+            setFilter('complete');
+          }}>Completed</button>
+
+          <button className="clear-completed" onClick={() => {
+            dispatch({ type: ACTIONS.deleteCOMPLETED })
+          }}>Clear Completed</button>
+        </footer>
+      </main>
     </div>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function TodoList(props) {
+    return (
+        <div className="task-indiv">
+            <button className={`task-done ${props.isComplete ? 'active-state' : ''}`} onClick={() => {
+                if (!props.isComplete) {
+                    props.handleDone(props.ID)
+                }
+            }
+            }>
+                <img src='/images/icon-check.svg' alt="" />
+            </button>
+            <div className={`task-name ${props.isComplete ? 'strike' : ''}`}>{props.task}</div>
+            <button className='task-delete' onClick={() => { props.handleDel(props.ID) }}>
+                <img src='/images/icon-cross.svg' alt="" />
+            </button>
+        </div>
+    )
+}
